3a477a6ab376b82d9f362648286bb110957da717
Biegon <156933863+Bigoncode@users.noreply.github.com>
2025-02-20 17:00:07 +0300

    initial commit

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -12,7 +12,7 @@ const Navbar = () => {
   return (
     <nav
       className={`${styles.paddingX}  w-full flex items-center py-6 fixed top-0 z-20
-       bg-primary [will-change:transform]`}
+       bg-primary overflow-hidden`}
     >
       <div
         className="w-full flex justify-between items-center 
